e5a623dedbb5afac7453d7f745f092216fdbb16b
Bishnu Dev <61447680+bordernone@users.noreply.github.com>
2022-09-28 17:38:11 +0400

    :sparkles: added background, interactive bubbles

--- Project1/script.js
+++ Project1/script.js
@@ -1,6 +1,7 @@
 let btn;
 let inputElement;
 let outputElement;
+let p5Instance;
 
 let wordsList = [];
 
@@ -10,7 +11,9 @@ window.onload = () => {
     inputElement = document.getElementById("word-input");
     outputElement = document.getElementById("output");
 
-    btn.addEventListener("click", generateSimilarWords);
+    btn.addEventListener("click", (e) => {
+        generateSimilarWords(e, getInput());
+    });
 };
 
 // Get input from the input field. If silent is false, it will alert user if the input field is empty; default is false.
@@ -27,11 +30,13 @@ const getInput = (silent = false) => {
 };
 
 // Fetch related words from the API and process the returned json to generate array of words.
-const generateSimilarWords = async (e) => {
-    e.preventDefault();
+const generateSimilarWords = async (e, input) => {
+    if (e) {
+        e.preventDefault();
+    }
 
     const queryObj = {
-        ml: getInput(),
+        ml: input,
     };
 
     // turn javascript object to url friendly string
@@ -42,14 +47,14 @@ const generateSimilarWords = async (e) => {
     try {
         let wordsData = await (await fetch(API_URL)).json();
 
-        console.log(wordsData);
-
         // Converts an array of objects to array of words
         wordsList = wordsData.map((item) => {
             return item.word;
         });
 
-        let p5_ = new p5(s);
+        if (!p5Instance) {
+            p5Instance = new p5(s);
+        }
     } catch (e) {
         console.log("Error: ", e);
     }
@@ -58,6 +63,10 @@ const generateSimilarWords = async (e) => {
 const s = (sketch) => {
     let canvas;
     let center;
+
+    let allBubbles = [];
+    let lastWordsList = [];
+
     sketch.setup = () => {
         canvas = sketch.createCanvas(
             outputElement.clientWidth,
@@ -71,26 +80,192 @@ const s = (sketch) => {
     };
 
     sketch.draw = () => {
-        (wordsList[0]) && drawBubble(center[0], center[1], 200, wordsList[0]);
-        (wordsList[1]) && drawBubble(center[0] - 150, center[1] - 150, 150, wordsList[1]);
-        (wordsList[2]) && drawBubble(center[0] - 150, center[1] + 150, 150, wordsList[2]);
-        (wordsList[3]) && drawBubble(center[0] + 150, center[1] - 150, 120, wordsList[3]);
-        (wordsList[4]) && drawBubble(center[0] + 150, center[1] + 150, 120, wordsList[4]);
-        (wordsList[5]) && drawBubble(center[0] - 160, center[1], 110, wordsList[5]);
-        (wordsList[6]) && drawBubble(center[0] + 160, center[1], 60, wordsList[6]);
-        (wordsList[7]) && drawBubble(center[0], center[1] - 140, 60, wordsList[7]);
+        if (hasWordsListChanged()) {
+            initializeBubbles();
+            lastWordsList = [...wordsList];
+        }
+        sketch.clear();
+        for (const [index, bubble] of allBubbles.entries()) {
+            bubble.draw(wordsList[index]);
+        }
     };
 
-    drawBubble = (x, y, r, word) => {
-        // sketch.stroke(0);
-        sketch.strokeWeight(0);
-        sketch.fill("#9999ff");
-        sketch.circle(x, y, r);
+    sketch.mouseClicked = () => {
+        for (const bubble of allBubbles) {
+            if (bubble.clicked(sketch.mouseX, sketch.mouseY)) bubble.onClick();
+        }
+        return false;
+    };
 
-        sketch.fill("#ff9933");
-        sketch.textSize(Math.floor((1.5 * r)/word.length));
-        sketch.textFont("monospace")
-        sketch.textAlign(sketch.CENTER, sketch.CENTER);
-        sketch.text(word, x, y);
+    initializeBubbles = () => {
+        allBubbles = [];
+
+        let bubblesLocation = [
+            [center[0], center[1], 200],
+            [center[0] - 150, center[1] - 150, 150],
+            [center[0] - 150, center[1] + 150, 150],
+            [center[0] + 150, center[1] - 150, 120],
+            [center[0] + 150, center[1] + 150, 120],
+            [center[0] - 160, center[1], 110],
+            [center[0] + 160, center[1], 60],
+            [center[0], center[1] - 140, 60],
+        ];
+
+        for (let i = 0; i < 8; i++) {
+            let x = bubblesLocation[i][0];
+            let y = bubblesLocation[i][1];
+            let r = bubblesLocation[i][2];
+            let temp = new Bubble(sketch, x, y, r);
+            allBubbles.push(temp);
+        }
+    };
+
+    hasWordsListChanged = () => {
+        return JSON.stringify(wordsList) !== JSON.stringify(lastWordsList);
     };
 };
+
+class Bubble {
+    constructor(s, x, y, diameter, word = null) {
+        this.sketch = s;
+
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.word = word;
+
+        this.colors = new Colors();
+        this.color = this.colors.generateRandomColor();
+
+        this.currentDiameter = 0;
+
+        this.lifeCycle = "null";
+    }
+
+    draw(word = this.word) {
+        if (word != this.word) this.word = word;
+        if (this.lifeCycle === "null") this.grow();
+        if (this.lifeCycle === "live") this.live();
+        if (this.lifeCycle === "dying") this.shrink();
+    }
+
+    async grow() {
+        this.sketch.strokeWeight(0);
+        let diameter = Math.min(this.currentDiameter, this.diameter);
+        this.sketch.fill(this.color);
+        this.sketch.circle(this.x, this.y, diameter);
+
+        this.sketch.fill(
+            this.colors.textColor(this.color[0], this.color[1], this.color[2])
+        );
+        let textSize = Math.floor((1.5 * diameter) / this.word.length);
+        if (textSize !== 0) {
+            this.sketch.textSize(
+                Math.floor((1.5 * diameter) / this.word.length)
+            );
+            this.sketch.textFont("monospace");
+            this.sketch.textAlign(this.sketch.CENTER, this.sketch.CENTER);
+            this.sketch.text(this.word, this.x, this.y);
+        }
+
+        this.currentDiameter += 3;
+        if (diameter === this.diameter) {
+            this.lifeCycle = "live";
+        }
+    }
+
+    live() {
+        this.sketch.strokeWeight(0);
+        this.sketch.fill(this.color);
+        this.sketch.circle(this.x, this.y, this.diameter);
+
+        this.sketch.fill(
+            this.colors.textColor(this.color[0], this.color[1], this.color[2])
+        );
+        let textSize = Math.floor((1.5 * this.diameter) / this.word.length);
+
+        this.sketch.textSize(textSize);
+        this.sketch.textFont("monospace");
+        this.sketch.textAlign(this.sketch.CENTER, this.sketch.CENTER);
+        this.sketch.text(this.word, this.x, this.y);
+    }
+
+    async shrink() {
+        this.sketch.strokeWeight(0);
+        let diameter = Math.min(this.currentDiameter, this.diameter);
+        this.sketch.fill(this.color);
+        this.sketch.circle(this.x, this.y, diameter);
+
+        this.sketch.fill(
+            this.colors.textColor(this.color[0], this.color[1], this.color[2])
+        );
+        let textSize = Math.floor((1.5 * diameter) / this.word.length);
+        if (textSize !== 0) {
+            this.sketch.textSize(
+                Math.floor((1.5 * diameter) / this.word.length)
+            );
+            this.sketch.textFont("monospace");
+            this.sketch.textAlign(this.sketch.CENTER, this.sketch.CENTER);
+            this.sketch.text(this.word, this.x, this.y);
+        }
+
+        this.currentDiameter -= 3;
+        if (this.currentDiameter <= 0) {
+            this.lifeCycle = "destroyed";
+            this.createNew();
+        }
+    }
+
+    clicked(clickX, clickY) {
+        if (
+            this.sketch.dist(clickX, clickY, this.x, this.y) <=
+            this.diameter / 2
+        )
+            return true;
+        return false;
+    }
+
+    onClick() {
+        this.lifeCycle = "dying";
+    }
+
+    createNew() {
+        generateSimilarWords(undefined, this.word);
+        console.log(this.word);
+    }
+}
+
+class Colors {
+    constructor() {
+        this.pastColors = new Set();
+    }
+
+    intensity(red, green, blue) {
+        return 1 - (0.299 * red + 0.587 * green + 0.114 * blue) / 255;
+    }
+
+    randomNum() {
+        return Math.floor(Math.random() * 256);
+    }
+
+    generateRandomColor(noRepeat = true) {
+        let red = this.randomNum();
+        let green = this.randomNum();
+        let blue = this.randomNum();
+
+        let color = [red, green, blue];
+        if (noRepeat) {
+            while (this.pastColors.has(color)) {
+                color = [this.randomNum(), this.randomNum(), this.randomNum()];
+            }
+        }
+
+        this.pastColors.add(color);
+        return color;
+    }
+
+    textColor(r, g, b) {
+        if (this.intensity(r, g, b) < 0.5) return "#000000";
+        return "#ffffff";
+    }
+}
