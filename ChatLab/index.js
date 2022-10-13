3cc118784f952046052edfd0ff1463ecad6b854c
Bishnu Dev <61447680+bordernone@users.noreply.github.com>
2022-10-13 12:27:46 +0400

    :sparkles: added db for chatlab

--- ChatLab/index.js
+++ ChatLab/index.js
@@ -2,32 +2,58 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+const Datastore = require("nedb"),
+    db = new Datastore("chats.db");
+
+db.loadDatabase();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-let data = [];
-
 app.post("/message", (req, res) => {
     const { username, message } = req.body;
     if (username && message) {
-        data = [...data, { username, message }];
-        res.json({
-            time: +new Date(),
-            data: data,
-        });
+        db.insert(
+            { username, message, createdAt: +new Date() },
+            function (err) {
+                if (err) {
+                    res.sendStatus(500).send("Database failed!");
+                } else {
+                    db.find({})
+                        .sort({ createdAt: 1 })
+                        .exec((err, docs) => {
+                            if (err) {
+                                res.sendStatus(500).send("Database failed!");
+                            } else {
+                                res.json({
+                                    time: +new Date(),
+                                    data: docs,
+                                });
+                            }
+                        });
+                }
+            }
+        );
     } else {
         res.sendStatus(403).send("Username or Message undefined");
     }
 });
 
 app.get("/messages", (req, res) => {
-    res.json({
-        time: +new Date(),
-        data: data,
-    });
+    db.find({})
+        .sort({ createdAt: 1 })
+        .exec((err, docs) => {
+            if (err) {
+                res.sendStatus(500).send("Database failed!");
+            } else {
+                res.json({
+                    time: +new Date(),
+                    data: docs,
+                });
+            }
+        });
 });
 
 app.use("/", express.static("public"));
